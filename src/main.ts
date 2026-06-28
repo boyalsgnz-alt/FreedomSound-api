0e65cb1dd6a3fe8c49c4fdcf0ecba7f8012e2f86
Gaëtan Boyals <boyalsgnz@gmail.com>
2026-06-28 16:02:46 +0800

    scaffolding swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('FreedomSound')
+    .setDescription('The API documentation build for FreedomSound')
+    .setVersion('1.0')
+    .addTag('freedom-sound')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
