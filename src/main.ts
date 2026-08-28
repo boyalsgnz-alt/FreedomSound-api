df75d6d41aacd85bdc20a9317b74232d8540a139
Gaëtan Boyals <boyalsgnz@gmail.com>
2026-08-28 18:21:29 +0800

    emitting event when processing files + making use of env file for API port

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  console.log(`API will now listen on ${process.env.API_PORT ?? 3000}`);
+  await app.listen(process.env.API_PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
